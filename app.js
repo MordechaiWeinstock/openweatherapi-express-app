deee7ce21bceb5479d0dd09386d7790c6e312b14
Mordechai Weinstock <gefenemail@gmail.com>
2020-08-16 18:12:41 +0100

    getting ready to deploy on Heroku

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.get('*', (get, res) => {
     res.send("Page not found");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT ||3000, () => {
     console.log("server is running on port 3000...");
 });
